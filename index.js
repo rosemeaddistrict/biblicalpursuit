66174ebc4a7bc29d21b27141a7cfe87610eb7353
bobkkblue <chienyec@gmail.com>
2025-02-16 20:53:01 -0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function displayTitle(){
-    var  curWeek = '2.09.2025 - 2.15.2025';
+    var  curWeek = '2.16.2025 - 2.22.2025';
     const title = `<h1>本周(${curWeek})晨兴与读经进度</h1><hr>`;
     document.getElementById("title").innerHTML = title;
 }
 
 function displayMorningRevival(){
     const title = '<h2>晨兴圣言</h2>';
-    const book = '2024年夏季训练 经历、享受并彰显基督（一）';
-    const chapter = "第十一周 在父的家里—活在那借着基督的死与复活所为我们预备的地方"
-    const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.04.%E5%A4%8F%E5%AD%A3%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%8D%81%E4%B8%80%E5%91%A8";
+    const book = '2024年秋季长老负责弟兄训练 活在神国的实际里';
+    const chapter = "第一周 神的国—神圣生命的范围以及神圣种类的范围"
+    const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.05.%E7%A7%8B%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E4%B8%80%E5%91%A8";
     const content = `<h3><ul><li>书目: ${book}</li><li>进度: <a href=${link}>${chapter}</a></li></ul></h3>`;
     document.getElementById("morningrevival").innerHTML = title + content;
 }
@@ -16,11 +16,11 @@ function displayMorningRevival(){
 function displayBible(){
     const title = '<h2>圣经</h2>';
     const old_test = "创世记";
-    const old_test_cov = "第29~33章";
-    const old_test_link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=1&f_ChapterNo=29";
+    const old_test_cov = "第34~38章";
+    const old_test_link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=1&f_ChapterNo=34";
     const new_test = "马可福音";
-    const new_test_cov = "第1~5章";
-    const new_test_link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=41&f_ChapterNo=1";
+    const new_test_cov = "第6~10章";
+    const new_test_link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=41&f_ChapterNo=6";
     const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a></li></ul></h3>`;
     document.getElementById("bible").innerHTML = title + content;
 }
@@ -28,9 +28,9 @@ function displayBible(){
 function displayLifeStudy(){
     const title = '<h2>生命读经</h2>';
     const book = "罗马书生命读经";
-    const coverage = "第17~24篇";
+    const coverage = "第25~32篇";
     const link = "https://simplified.lsmchinese.org/lifestudy/roma-idx.html";
-    const date = "2/13/2025";
+    const date = "3/13/2025";
     const content = `<h3><ul><li>进度: ${book} <a href=${link}>${coverage}</a></li><li>展览日期: ${date}</li></ul></h3>`;
     document.getElementById("lifestudy").innerHTML = title + content;
 }
