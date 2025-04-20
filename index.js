e923ce7f664af8cfc320f50ada98ec57a95a6d6b
bobkkblue <chienyec@gmail.com>
2025-04-20 13:25:24 -0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function displayTitle(){
 function displayMorningRevival(){
     const title = '<h2>晨兴圣言</h2>';
     const book = '2024年冬季训练 经历、享受并彰显基督（二）';
-    const chapter = "第一周 大卫的后裔成为神的儿子"
+    const chapter = "第二周 基督作为释放者以及使我们得胜有余的那一位"
     const link = curWeekLink;
     //const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.05.%E7%A7%8B%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8";
     const content = `<h3><ul><li>书目: ${book}</li><li>进度: <a href=${link}>${chapter}</a></li></ul></h3>`;
@@ -19,10 +19,10 @@ function displayMorningRevival(){
 function displayBible(){
     const title = '<h2>圣经</h2>';
     const old_test = "出埃及记";
-    const old_test_cov = "第24~28章";
+    const old_test_cov = "第29~33章";
     const old_test_link = curWeekLink;
     const new_test = "约翰福音";
-    const new_test_cov = "第6~10章";
+    const new_test_cov = "第11~15章";
     const new_test_link = curWeekLink;
     const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a></li></ul></h3>`;
     document.getElementById("bible").innerHTML = title + content;
@@ -31,9 +31,9 @@ function displayBible(){
 function displayLifeStudy(){
     const title = '<h2>生命读经</h2>';
     const book = "马太福音生命读经";
-    const coverage = "第1~6篇, 13, 14篇";
+    const coverage = "第15~22篇";
     const link = curWeekLink;
-    const date = "4/17/2025";
+    const date = "5/22/2025";
     const content = `<h3><ul><li>进度: ${book} <a href=${link}>${coverage}</a></li><li>展览日期: ${date}</li></ul></h3>`;
     document.getElementById("lifestudy").innerHTML = title + content;
 }
