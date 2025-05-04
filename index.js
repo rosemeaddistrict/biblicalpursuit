a8b18ba354d0ea63d89a43dc1a7e3a5c634f8021
bobkkblue <chienyec@gmail.com>
2025-05-04 16:56:59 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const curWeekLink = "https://chenin0423.synology.me:5001/";
 
 function displayTitle(){
-    var  curWeek = '4.27.2025 - 5.03.2025';
+    var  curWeek = '5.04.2025 - 5.10.2025';
     const title = `<h1>本周(${curWeek})晨兴与读经进度</a></h1><hr>`;
     document.getElementById("title").innerHTML = title;
 }
@@ -9,7 +9,7 @@ function displayTitle(){
 function displayMorningRevival(){
     const title = '<h2>晨兴圣言</h2>';
     const book = '2024年冬季训练 经历、享受并彰显基督（二）';
-    const chapter = "第三周 神的能力与神的智慧"
+    const chapter = "第四周 我们的灵食、我们的灵磐石以及主筵席的内容"
     const link = curWeekLink;
     //const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.05.%E7%A7%8B%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8";
     const content = `<h3><ul><li>书目: ${book}</li><li>进度: <a href=${link}>${chapter}</a></li></ul></h3>`;
@@ -19,12 +19,16 @@ function displayMorningRevival(){
 function displayBible(){
     const title = '<h2>圣经</h2>';
     const old_test = "出埃及记";
-    const old_test_cov = "第34~38章";
+    const old_test_cov = "第39~40章";
+    const old_test2 = "利未记";
+    const old_test_cov2 = "第1~3章";
     const old_test_link = curWeekLink;
     const new_test = "约翰福音";
-    const new_test_cov = "第16~20章";
+    const new_test_cov = "第21章";
+    const new_test2 = "使徒行传";
+    const new_test_cov2 = "第1~4章";
     const new_test_link = curWeekLink;
-    const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a></li></ul></h3>`;
+    const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a> ${old_test2} <a href=${old_test_link}>${old_test_cov2}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a> ${new_test2} <a href=${new_test_link}>${new_test_cov2}</a></li></ul></h3>`;
     document.getElementById("bible").innerHTML = title + content;
 }
 
