b7acd2f71f28f44020ca5e99898f30a00a5e89f8
bobkkblue <chienyec@gmail.com>
2025-06-01 13:36:49 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const curWeekLink = "https://chenin0423.synology.me:5001/";
 
 function displayTitle(){
-    var  curWeek = '5.25.2025 - 5.31.2025';
+    var  curWeek = '6.1.2025 - 6.7.2025';
     const title = `<h1>本周(${curWeek})晨兴与读经进度</a></h1><hr>`;
     document.getElementById("title").innerHTML = title;
 }
@@ -9,7 +9,7 @@ function displayTitle(){
 function displayMorningRevival(){
     const title = '<h2>晨兴圣言</h2>';
     const book = '2024年冬季训练 经历、享受并彰显基督（二）';
-    const chapter = "第七周 成形在信徒里面的那一位"
+    const chapter = "第八周 接受、经历并享受包罗万有之基督作为包罗万有赐生命之灵（就是神全备福音那包括一切之福的集大成）的路"
     const link = curWeekLink;
     //const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.05.%E7%A7%8B%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8";
     const content = `<h3><ul><li>书目: ${book}</li><li>进度: <a href=${link}>${chapter}</a></li></ul></h3>`;
@@ -19,10 +19,10 @@ function displayMorningRevival(){
 function displayBible(){
     const title = '<h2>圣经</h2>';
     const old_test = "利未记";
-    const old_test_cov = "第14~18章";
+    const old_test_cov = "第19~23章";
     const old_test_link = curWeekLink;
     const new_test = "使徒行传";
-    const new_test_cov = "第15~19章";
+    const new_test_cov = "第20~24章";
     const new_test_link = curWeekLink;
     const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a></li></ul></h3>`;
     document.getElementById("bible").innerHTML = title + content;
