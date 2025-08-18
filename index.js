27d26e3ac9307d20cdbbf16cf8d43ba641a4e0fd
bobkkblue <chienyec@gmail.com>
2025-08-17 21:06:08 -0700

    update

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function displayTitle(){
 
 function displayMorningRevival(){
     const title = '<h2>晨兴圣言</h2>';
-    const book = '2025年国际华语特会 罗马书五至八章—圣经的核仁';
-    const chapter = "第五周 内住的基督"
+    const book = '2025年春季长老负责弟兄训练 同心合意以及使徒行传极重要的方面';
+    const chapter = "第一周 使徒行传的继续—基督团体的继续"
     const link = curWeekLink;
     //const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.05.%E7%A7%8B%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8";
     const content = `<h3><ul><li>书目: ${book}</li><li>进度: <a href=${link}>${chapter}</a></li></ul></h3>`;
