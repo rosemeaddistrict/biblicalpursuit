706a6dde79a3b2c356c76f4751076075078f47d2
bobkkblue <chienyec@gmail.com>
2025-09-07 22:25:23 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const curWeekLink = "https://chenin0423.synology.me:5001/";
 
 function displayTitle(){
-    var  curWeek = '8.31.2025 - 9.06.2025';
+    var  curWeek = '9.07.2025 - 9.13.2025';
     const title = `<h1>本周(${curWeek})晨兴与读经进度</a></h1><hr>`;
     document.getElementById("title").innerHTML = title;
 }
@@ -9,7 +9,7 @@ function displayTitle(){
 function displayMorningRevival(){
     const title = '<h2>晨兴圣言</h2>';
     const book = '2025年春季长老负责弟兄训练 同心合意以及使徒行传极重要的方面';
-    const chapter = "第三周 借着主恢复中能力的三个实质—祷告、那灵与话—维持同心合意"
+    const chapter = "第四周 复活、升天、包罗万有的基督"
     const link = curWeekLink;
     //const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.05.%E7%A7%8B%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8";
     const content = `<h3><ul><li>书目: ${book}</li><li>进度: <a href=${link}>${chapter}</a></li></ul></h3>`;
@@ -19,12 +19,12 @@ function displayMorningRevival(){
 function displayBible(){
     const title = '<h2>圣经</h2>';
     const old_test = "申命记";
-    const old_test_cov = "第21~25章"
+    const old_test_cov = "第26~30章"
     const old_test_link = curWeekLink;
-    const new_test = "以弗所书";
-    const new_test_cov = "第6章";
-    const new_test1 = "腓立比书";
-    const new_test_cov1 = "第1~4章";
+    const new_test = "歌罗西书";
+    const new_test_cov = "第1~4章";
+    const new_test1 = "帖撒罗尼迦前书";
+    const new_test_cov1 = "第1章";
     const new_test_link = curWeekLink;
     const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a> ${new_test1} <a href=${new_test_link}>${new_test_cov1}</a></li></ul></h3>`;
     document.getElementById("bible").innerHTML = title + content;
