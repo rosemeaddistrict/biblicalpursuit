c258ed836c1e74ca9e3255fe15930918d4cbf435
bobkkblue <chienyec@gmail.com>
2025-10-19 22:56:49 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const curWeekLink = "https://chenin0423.synology.me:5001/";
 
 function displayTitle(){
-    var  curWeek = '10.12.2025 - 10.18.2025';
+    var  curWeek = '10.19.2025 - 10.25.2025';
     const title = `<h1>本周(${curWeek})晨兴与读经进度</a></h1><hr>`;
     document.getElementById("title").innerHTML = title;
 }
@@ -9,7 +9,7 @@ function displayTitle(){
 function displayMorningRevival(){
     const title = '<h2>晨兴圣言</h2>';
     const book = '2025年夏季训练 经历、享受并彰显基督（三）';
-    const chapter = "第一周 基督为我们的美德、神的平安、我们的秘诀和那加我们能力者"
+    const chapter = "第二周 认识基督作我们的榜样并以祂为榜样"
     const link = curWeekLink;
     //const link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.05.%E7%A7%8B%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8";
     const content = `<h3><ul><li>书目: ${book}</li><li>进度: <a href=${link}>${chapter}</a></li></ul></h3>`;
@@ -19,14 +19,14 @@ function displayMorningRevival(){
 function displayBible(){
     const title = '<h2>圣经</h2>';
     const old_test = "约书亚记";
-    const old_test_cov = "第17~21章"
+    const old_test_cov = "第22~24章"
+    const old_test1 = "士师记";
+    const old_test_cov1 = "第1~2章"
     const old_test_link = curWeekLink;
-    const new_test = "腓利门书";
-    const new_test_cov = "第1章";
-    const new_test1 = "希伯来书";
-    const new_test_cov1 = "第1~4章";
+    const new_test = "希伯来书";
+    const new_test_cov = "第5~9章";
     const new_test_link = curWeekLink;
-    const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a> ${new_test1} <a href=${new_test_link}>${new_test_cov1}</a></li></ul></h3>`;
+    const content = `<h3><ul><li>旧约: ${old_test} <a href=${old_test_link}>${old_test_cov}</a> ${old_test1} <a href=${old_test_link}>${old_test_cov1}</a></li><li>新约: ${new_test} <a href=${new_test_link}>${new_test_cov}</a></li></ul></h3>`;
     document.getElementById("bible").innerHTML = title + content;
 }
 
